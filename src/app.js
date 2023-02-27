bf98f21463a2cce611e959a13d92b0933dc73c67
allynef <allynehf@gmail.com>
2023-02-27 16:27:56 -0300

    added code to weather app/city functionality

--- src/app.js
+++ src/app.js
@@ -40,61 +40,65 @@ let year = now.getFullYear();
 
 let formatDate = `${day}, ${month} ${monthdate}, ${hours}:${minutes}`;
 
-let currentTime = document.querySelector("#current-time");
+let currentTime = document.querySelector("#date");
 currentTime.innerHTML = formatDate;
 
 //Code to make an API call to bring City and weather from OpenWeatherMap
 
-function search(event) {
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
   let apiKey = "6ac39b605f8239ad2b0b01c884c8b5d0";
-  let city = "Campinas";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-//Challenge 2 - Change the name of the City when searching
-function changeCity(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-city");
-  let newCity = document.querySelector("#current-city");
-  newCity.innerHTML = `${searchInput.value}`;
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
 }
 
-let searchCity = document.querySelector("#search-city");
-searchCity.addEventListener("submit", changeCity);
+function searchLocation(position) {
+  let apiKey = "6ac39b605f8239ad2b0b01c884c8b5d0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-// function changeCity(event) {
-//   event.preventDefault();
-//   let searchForm = document.querySelector("#search-form");
-//   let newCity = document.querySelector("#city-display");
-//   newCity.innerHTML = `${searchForm.value}`;
-// }
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
 
-// let searchCity = document.querySelector("#search-city");
-// searchCity.addEventListener("click", changeCity);
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
 
-// function changeCity(event) {
-//   event.preventDefault();
-//   let searchInput = document.querySelector("#search-city");
-//   let newCity = document.querySelector("#current-city");
-//   newCity.innerHTML = `${searchInput.value}`;
-// }
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
 
-//Challenge 3 - Change the temperature from Celsius to Fahrenheit
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
 
-// function changeToCelsius(event) {
-//   event.preventDefault();
-//   let celsiusTemp = document.getElementById("current-temp");
-//   celsiusTemp.innerHTML = "21°";
-// }
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
 
-// function changeToFahren(event) {
-//   event.preventDefault();
-//   let fahrenTemp = document.getElementById("current-temp");
-//   fahrenTemp.innerHTML = "65°";
-// }
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
-// let celsiusClick = document.querySelector("#cels-temp");
-// celsiusClick.addEventListener("click", changeToCelsius);
-
-// let fahrenClick = document.querySelector("#fahren-temp");
-// fahrenClick.addEventListener("click", changeToFahren);
+searchCity("New York");
